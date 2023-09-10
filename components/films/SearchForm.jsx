78f543960ea531c62c4b4bb131300c06360ea16e
АннаОнис <onischenko.anna11@gmail.com>
2023-09-11 02:37:21 +0300

    поиск фильмов

--- components/films/SearchForm.jsx
+++ components/films/SearchForm.jsx
@@ -10,15 +10,15 @@ export default function SearchForm() {
 
     async function getFilms(text) {
         try {
-            if(text == '') throw new Error('Вы не ввели название фильма!');
-            else{
-                setError(null);
-                const response = await fetch('http://www.omdbapi.com/?apikey=53ce0bd4&s=' + text);
-                if (!response.ok) throw new Error('fetch ' + response.status);
-                let rezult = await response.json();
-                setFilms(rezult.Search);
-                console.log(films);              
-            }
+            setError(null);
+            
+            if (text == '') throw new Error('Вы не ввели название фильма!');
+
+            const response = await fetch('http://www.omdbapi.com/?apikey=53ce0bd4&s=' + text);
+            if (!response.ok) throw new Error('fetch ' + response.status);
+            let rezult = await response.json();
+            setFilms(rezult.Search);
+            console.log(films);
 
         } catch (err) {
             setError(err);
@@ -27,18 +27,18 @@ export default function SearchForm() {
 
     return <>
         <div >
-            <input 
-                type="search" 
+            <input
+                type="search"
                 className={styles.searchInput}
-                value={value} 
-                onInput={evt => setValue(evt.target.value)} 
+                value={value}
+                onInput={evt => setValue(evt.target.value)}
                 placeholder='Введите название фильма для поиска...'
-                ></input>
-            <button 
-                className={styles.searchButton} 
+            ></input>
+            <button
+                className={styles.searchButton}
                 onClick={() => getFilms(value)}
-                >Найти</button>            
+            >Найти</button>
         </div>
-        <Films films={films} error={error}/>
+        <Films films={films} error={error} />
     </>
 }
